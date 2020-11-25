970a7dd76d101ab3c3703f0949e6b022c60b1e71
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-25 18:19:18 +0000

    Initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
-import "./styles/App.scss";
+import "./styles/app.scss";
 
 function App() {
   return (
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
@@ -8,7 +14,15 @@ const Player = () => {
         <input type="range" />
         <p>End time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
     </div>
   );
 };
