2286881c057ea26a423f5c1f8a2c360fa4d5b454
Giorgio Torre <giorgio.torre8@gmail.com>
2020-11-26 13:48:36 +0000

    passed currentSong to Song component. added some initial styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
 import data from "./data";
 import "./styles/app.scss";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="box">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
